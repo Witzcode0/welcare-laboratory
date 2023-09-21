cc62a52c87dc692710bc8a4c1479a8f99c13c2a0
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2023-09-21 15:05:15 +0530

    permission issuses done

---
+++ authenticate/context_processors.py
@@ -0,0 +1,29 @@
+from main.models import EmployeePermissionRelation
+
+def authenticated_employee(request):
+    # Check if the user is authenticated
+    if hasattr(request, 'employee'):
+        # You can add any additional data you need here
+        employee_permissions = EmployeePermissionRelation.objects.filter(employee=request.employee)
+        
+        # Create an array of employee data
+        employee_data = {
+            'id': request.employee.id,
+            'first_name': request.employee.first_name,
+            'last_name': request.employee.last_name,
+            'permissions': []
+
+        }
+
+        for permission_ in employee_permissions:
+            permission_dict = {}
+            permission_dict[str(permission_.permission)] = permission_.permission_status
+            employee_data['permissions'].append(permission_dict)
+
+        print(employee_data)
+
+        return {
+            'authenticated_employee': employee_data,
+            'employee_permissions': employee_permissions,
+        }
+    return {}
--- lab/settings.py
+++ lab/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'dashboard.context_processors.employee_data'
+                'authenticate.context_processors.authenticated_employee'
             ],
         },
     },
